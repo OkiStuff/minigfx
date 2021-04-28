accf4ba617f0ce5265aae583795753ba189c15b8
laurenloco <laurenloco06@gmail.com>
2021-04-28 19:13:32 -0300

    Modify keyboard input example

--- examples/keyboard.c
+++ examples/keyboard.c
@@ -1,17 +1,31 @@
 #define MINIGFX_IMPLEMENTATION
 #include "../minigfx.h"
-#include <stdio.h>
 
 int main()
 {
     MiniGFX_InitWindow(1024, 600, "Basic keyboard input in minigfx");
+    Rectangle rec = (Rectangle){ 1024/2-64/2, 600/2-64/2, 64, 64 };
 
     while (!MiniGFX_WindowCloses()) {
-        if (MiniGFX_IsKeyPressed(KEY_P)) printf("Key P was pressed\n");
-        if (MiniGFX_IsKeyHeld(KEY_H)) printf("Key H was pressed\n");
+        if (MiniGFX_IsKeyHeld(KEY_W)) {
+            rec.y -= 4;
+        }
+
+        if (MiniGFX_IsKeyHeld(KEY_A)) {
+            rec.x -= 4;
+        }
+
+        if (MiniGFX_IsKeyHeld(KEY_S)) {
+            rec.y += 4;
+        }
+
+        if (MiniGFX_IsKeyHeld(KEY_D)) {
+            rec.x += 4;
+        }
 
         MiniGFX_StartDrawing();
         MiniGFX_ClearTo(WHITE);
+        MiniGFX_DrawRectangleRec(rec, RED);
         MiniGFX_EndDrawing();
     }
 
