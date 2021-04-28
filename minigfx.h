1f01549f4d6fe5851b5c5d3006fadd14c6eb1c00
laurenloco <laurenloco06@gmail.com>
2021-04-27 22:40:53 -0300

    Add more colors

--- minigfx.h
+++ minigfx.h
@@ -14,7 +14,17 @@ typedef struct Color {
 } Color;
 
 // ------- Defines and macros -
-#define WHITE       (Color){ 255, 255, 255, 255 }
+#define WHITE           (Color){ 255, 255, 255, 255 }
+#define BLACK           (Color){ 0, 0, 0, 255       }
+
+#define RED             (Color){ 255, 50, 100, 255  }
+#define ORANGE          (Color){ 255, 153, 51, 255  }
+#define YELLOW          (Color){ 255, 201, 90, 255  }
+#define GREEN           (Color){ 102, 255, 102, 255 }
+#define BLUE            (Color){ 128, 255, 0, 255   }
+#define PURPLE          (Color){ 178, 102, 255, 255 }
+#define PINK            (Color){ 255, 204, 255, 255 }
+#define MAGENTA         (Color){ 255, 102, 255, 255 }
 
 // ------- Variables ---------
 GLFWwindow *window;
