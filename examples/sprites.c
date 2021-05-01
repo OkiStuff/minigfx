e5796b115e1eca59222296cec7fc12149fc72771
laurenloco <laurenloco06@gmail.com>
2021-05-01 15:15:32 -0300

    Modify sprites.c
    Add scaling example

--- examples/sprites.c
+++ examples/sprites.c
@@ -5,11 +5,18 @@ int main()
 {
     MiniGFX_CreateWindow(1024, 600, "Loading sprites in minigfx");
     Sprite image = MiniGFX_LoadSprite("../resources/header.png");
+	float scale = 1.0f;
 
     while (!MiniGFX_WindowCloses()) {
+		if (MiniGFX_IsKeyHeld(KEY_UP)) {
+			scale += 0.001;
+		} else if (MiniGFX_IsKeyHeld(KEY_DOWN)) {
+			scale -= 0.001;
+		}
+
         MiniGFX_StartDrawing();
         MiniGFX_ClearTo(BLACK);
-        MiniGFX_DrawSprite(image, -150, 70, 1.0, WHITE);
+        MiniGFX_DrawSprite(image, -150, 70, scale, WHITE);
         MiniGFX_StopDrawing();
     }
 
